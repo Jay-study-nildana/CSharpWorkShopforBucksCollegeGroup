1fe75de5ad9f262bf59b43c50da31086e7e74468
Jay <vijayasimhabr@live.com>
2023-06-07 18:57:23 +0530

    added hello world
    added a simple hello world project.
    
    also testing out github desktop to see if it works.

---
+++ TempFolder/HelloStudents1/HelloStudents1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Students!");
+var helloworld2 = "I am your tutor, Jay, from Mysore, India";
+Console.WriteLine(helloworld2);
